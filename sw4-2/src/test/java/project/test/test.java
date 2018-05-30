678a523bca481a02c0e5d304de80e2232ce86f17
chldlsxor <3185582@naver.com>
2018-05-30 17:31:37 +0900

    연결잘됨

--- sw4-2/src/test/java/project/test/test.java
+++ sw4-2/src/test/java/project/test/test.java
@@ -1,5 +1,40 @@
 package project.test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import project.bean.MemberDto;
+import project.repository.MemberDao;
+
+//192.168.0.170:9090/apex/f?p=4500
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:/spring/application-config.xml")
 public class test {
 
+	@Autowired MemberDao memberDao;
+	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Test
+	public void register() {
+		log.info("memberDao = {}", memberDao);
+		String id = "idid";
+		String name = "namename";
+		String nick = "nicknick";
+		String pw = "pwpw";
+		
+		MemberDto memberDto = new MemberDto();
+		memberDto.setId(id);
+		memberDto.setName(name);
+		memberDto.setNick(nick);
+		memberDto.setPw(pw);
+		
+		memberDao.register(memberDto);
+	}
 }
