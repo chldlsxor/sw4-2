2ff7bf6b3e8d2c4935eec33aa4542e703bdfa4ce
204-16 <204-16@DESKTOP-RKNIIMB>
2018-06-05 21:43:37 +0900

    메세지 수신여부 판단
    full 시그널 안가는거 같음 ㅗㅗ

--- sw4-2/src/main/java/project/websocket/WebSocketServer.java
+++ sw4-2/src/main/java/project/websocket/WebSocketServer.java
@@ -2,6 +2,7 @@ package project.websocket;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,9 +30,22 @@ public class WebSocketServer extends TextWebSocketHandler{
 		//user.add(session);
 		 String messageFrom = session.getAttributes().get("userid").toString();
 		 String messageTo = session.getAttributes().get("messageto").toString();		
-		 String gid = messageFrom+messageTo;
-		 user.put(gid, session);
-		 log.info("그룹 아이디 : {}", gid);
+		 user.put(messageFrom+messageTo, session);//내 메세지창에 session저장
+		 
+		 int count = 0;
+		 for(String gid : user.keySet()) {
+			 if(gid.equals(messageFrom+messageTo)||gid.equals(messageTo+messageFrom)) {
+				 count++;
+			 }
+		 }
+		 //채팅방에 사람이 다 찼을때 --> 신호 보내서 read class로 만들어!
+		 if(count==2) {
+			 String full_signal = UUID.nameUUIDFromBytes("full".getBytes()).toString();
+			 log.info("full의 uuid : {}",full_signal );
+			 TextMessage text = new TextMessage(full_signal);
+			 user.get(messageFrom+messageTo).sendMessage(text);//내방에 신호
+			 user.get(messageTo+messageFrom).sendMessage(text);//상대방에 신호
+		 }	
 	}
 	
 	@Override
@@ -45,7 +59,8 @@ public class WebSocketServer extends TextWebSocketHandler{
 			log.info("그룹 아이디  : {}",gid);
 			log.info("	- 웹소켓  : {}",user.get(gid));
 			log.info("	- 메세지  : {}",text.toString());
-			if(gid.equals(messageTo+messageFrom)) {//내가 나가면 나 말구 상대방 창에 메세지 보내기
+			if(gid.equals(messageTo+messageFrom)) {//gid가 (상대방-->나) 창이면
+				//내가 나가면 나 말구 상대방 창에 메세지 보내기
 				user.get(gid).sendMessage(text);
 				log.info("--> 메세지 전송 완료");
 			}		
@@ -62,7 +77,8 @@ public class WebSocketServer extends TextWebSocketHandler{
 		String messageFrom  = (String)session.getAttributes().get("userid");
 		String messageTo = (String)session.getAttributes().get("messageto");
 		String context = message.getPayload();
-		TextMessage text = new TextMessage("["+messageFrom+"] "+ context);
+		TextMessage sender = new TextMessage(messageFrom);
+		TextMessage content = new TextMessage(context);
 		
 		MessageDto messageDto = new MessageDto();
 		messageDto.setSend(messageFrom);
@@ -71,15 +87,31 @@ public class WebSocketServer extends TextWebSocketHandler{
 		messageService.sendMessage(messageDto);
 		
 		log.info("세션  : {}", session);
-		log.info("메세지  : {}",text.toString());
-		//for(WebSocketSession ws : user) {
+		//log.info("메세지  : {}",text.toString());
+		//.for(WebSocketSession ws : user) {
 		for(String gid : user.keySet()) {
 			log.info("그룹 아이디  : {}",gid);
 			log.info("	- 웹소켓  : {}",user.get(gid));
-			log.info("	- 메세지  : {}",text.toString());
-			if(gid.equals(messageFrom+messageTo)||gid.equals(messageTo+messageFrom)) {
-				user.get(gid).sendMessage(text);
+			log.info("	- 보낸이  : {}",sender.toString());
+			log.info("	- 보낸이  : {}",content.toString());
+			
+			if(gid.equals(messageFrom+messageTo)||gid.equals(messageTo+messageFrom)) {	//(나-->상대방), (상대방-->나) 창에 메세지 표시
+				user.get(gid).sendMessage(sender);
+				user.get(gid).sendMessage(content);
 				log.info("--> 메세지 전송 완료");
+				/*if(isconnected.get(messageFrom+messageTo)!=null&& gid.equals(messageFrom+messageTo)) {	// 상대방 창: 나랑 연결됨//(나-->상대방)
+					//내방, 상대방 창 메세지 보내기(둘다 read 표시 안함)
+					TextMessage text = new TextMessage("["+messageFrom+"] "+ context);
+					user.get(gid).sendMessage(text);//내방
+					user.get(messageTo+messageFrom).sendMessage(text);//상대방 
+					log.info("--> 메세지 전송 완료");
+				}
+				else if(isconnected.get(messageFrom+messageTo)==null&& gid.equals(messageFrom+messageTo)) { //상대방 창 : 나랑 연결 안됨//(나-->상대방)
+					//내방에만 메세지 보내기(read표시 1로 함)
+					TextMessage text = new TextMessage("["+messageFrom+"] "+ context+"(1)");
+					user.get(gid).sendMessage(text);
+					log.info("--> 메세지 전송 완료");
+				}*/
 			}		
 		}
 		
--- sw4-2/src/main/webapp/WEB-INF/view/send_message.jsp
+++ sw4-2/src/main/webapp/WEB-INF/view/send_message.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <html>
+
     <head>
         <title>웹소켓 문제</title>
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -9,6 +10,9 @@
         		max-height:50%;
         		overflow-y:scroll;
         	}
+        	.not-read{
+    			color: purple;
+    		}
         </style>
         
         <script src="http://code.jquery.com/jquery-latest.js"></script>
@@ -39,6 +43,9 @@
         		
         		window.websocket = new WebSocket(uri);
         		
+        		var ismassage= false;
+        		var send = null;
+        		
         		websocket.onopen = function(e){
         			$("#result").append("<h4>서버에 접속하였습니다</h4>");
         		};
@@ -49,7 +56,32 @@
         			$("#result").append("<h4>서버에 접속할 수 없습니다</h4>");
         		};
         		websocket.onmessage = function(e){
-        			$("#result").append("<h4>"+e.data+"</h4>");
+        			console.log("넘어온 메세지 : "+e.data);
+        			console.log("ismessage 값 : "+ ismassage);
+        			console.log("메세지 보낸 사람 아이디 : "+send);
+        			console.log("userid 값(현재 로그인 유저 ) : "+$("#userid").text().trim());
+        		
+        			if(e.date=="e9dc924f-238f-36cc-a946-5942875fe8f0"){
+        				//class not read를 remove
+        				console.log("정원참..");
+        				$( '#message' ).removeClass("not-read");
+        			}else if(!ismassage){
+        				ismassage = true;
+        				send = e.data;
+        			}
+        			else{
+        				ismassage = false;
+        				console.log(send==$("#userid").text().trim());
+        				if(send==$("#userid").text().trim()){
+        					$("#result").append("<div id='message' class = 'not-read' align = 'right'>"+e.data+"</div>")
+        				}
+        				else{
+        					$("#result").append("<div id='message' class = 'not-read' align = 'left'>"+e.data+"</div>")
+        				}
+        			}
+        			
+        			
+        			
         		};
         		
         	}
@@ -64,11 +96,11 @@
     <%@ taglib prefix="c" uri = "http://java.sun.com/jsp/jstl/core"%>
     <body>
     	<div class="container-fluid">
-    		<div class="jumbotron text-center">
+    		<div class="jumbotron text-center" >
     			<h1>메세지</h1>
     			
-    			<p>
-    				로그인 ID : ${userid }
+    			<p id = "userid">
+    				${userid }
     			</p>
     			<p>
     				messageFrom : ${userid }
@@ -79,21 +111,26 @@
     		</div>
     		<div class="row">
     			
-    		</div>
+    		</div>	
     		<div class="row">
+    			<div id="result" class="col-md-offset-1 col-md-10">
+    				<!-- DB리스트에 있는 메세지 리스트는 이 창이 열린 순간 다 읽음 처리 되므로 read 표시 안함 -->
+    				<c:forEach var = "message" items="${message_list}">
+    					<c:if test="${userid == message.send }">
+							<div align="right">[${message.send }] ${message.content }</div>
+						</c:if>
+						<c:if test="${messageto == message.send }">
+							<div align="left">[${message.send }] ${message.content }</div>
+						</c:if>
+					</c:forEach>
+    			</div>
+    		</div>
+    		<div class="row bottom">
     			<div class="col-md-offset-1 col-md-8">		
     				<input id="chat" type="text" placeholder="입력.." class="form-control">
     			</div>
     			<div class="col-md-2">
     				<input id="send" type="button" value="전송" class="btn btn-default btn-block">
-    			</div>
-    		</div>
-    		
-    		<div class="row">
-    			<div id="result" class="col-md-offset-1 col-md-10">
-    				<c:forEach var = "message" items="${message_list}">
-						<h4>[${message.send }] ${message.content } (${message.read })</h4>
-					</c:forEach>
     			</div>
     		</div>
     	</div>
